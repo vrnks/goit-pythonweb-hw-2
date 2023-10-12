884d2e181be6a265faf90b0422d4939a34e96591
a <likafloat@gmail.com>
2023-10-12 13:20:36 +0300

    Create jason.py

---
+++ web_2/jason.py
@@ -0,0 +1,27 @@
+import address_book, note_book, file_sort
+
+def main():
+
+    while True:
+
+        choice = input('1) Address book of your victims; 2) Notebook for special murders; 3) Sort files in some folder; 4) Exit. \nChoose program: ')
+
+        try:
+            choice = int(choice)
+        except ValueError as error:
+            print('Use only specified numbers!')
+
+        if  choice == 1:
+            address_book.main()
+        elif choice == 2:
+            note_book.main()
+        elif choice == 3:
+            file_sort.main()
+        elif choice == 4:
+            exit()
+        else:
+            print('Use only specified numbers!')
+
+if __name__ == '__main__':
+    main()
+
