b4d7781f57431f81252dff046c5c7ae24b7338f1
a <likafloat@gmail.com>
2023-10-12 13:20:42 +0300

    Create file_parser.py

---
+++ web_2/file_parser.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+
+JPEG_IMAGES = []
+JPG_IMAGES = []
+PNG_IMAGES = []
+SVG_IMAGES = []
+MP3_AUDIO = []
+MP4_VIDEO = []
+MY_OTHER = []
+ARCHIVES = []
+
+REGISTER_EXTENSION = {
+    'JPEG': JPEG_IMAGES,
+    'JPG': JPG_IMAGES,
+    'PNG': PNG_IMAGES,
+    'SVG': SVG_IMAGES,
+    'MP3': MP3_AUDIO,
+    'MP4': MP4_VIDEO,
+    'ZIP': ARCHIVES
+}
+
+FOLDERS = []
+EXTENSION = set()
+UNKNOWN = set()
+
+
+def get_extension(filename: str) -> str:
+    return Path(filename).suffix[1:].upper()
+
+
+def scan(folder: Path) -> None:
+    for item in folder.iterdir():
+        if item.is_dir():
+            if item.name not in ('archives', 'video', 'audio', 'documents', 'images', 'MY_OTHER'):
+                FOLDERS.append(item)
+                scan(item)
+            continue
+
+        ext = get_extension(item.name)
+        fullname = folder / item.name
+        if not ext:
+            MY_OTHER.append(fullname)
+        else:
+            try:
+                container = REGISTER_EXTENSION[ext]
+                EXTENSION.add(ext)
+                container.append(fullname)
+            except KeyError:
+                UNKNOWN.add(ext)
+                MY_OTHER.append(fullname)
